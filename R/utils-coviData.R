5e936f06bf2401ddde79597cfca9bcdaf4e957cd
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-01-12 20:39:55 -0600

    Add docs

---
+++ R/utils-coviData.R
@@ -0,0 +1,10 @@
+#' coviData
+#'
+#' The coviData package serves as a backend for many of the SCHD R packages
+#' related to COVID-19, including this package. Some functions that would
+#' otherwise be included in this package are loaded here.
+#'
+#' @importFrom coviData load_report_date path_inv path_pcr read_file_delim
+#'
+#' @name utils-coviData
+NULL
