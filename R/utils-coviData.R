1c947d3dff26c6ee13ba0d796d39440426876600
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-02-26 08:44:33 -0600

    Minimize direct imports from coviData
    Restrict to filesystem and standardization functions

--- R/utils-coviData.R
+++ R/utils-coviData.R
@@ -1,11 +1,11 @@
-#' coviData
+#' coviData Utilities
 #'
-#' The coviData package serves as a backend for many of the SCHD R packages
-#' related to COVID-19, including this package. Some functions that would
-#' otherwise be included in this package are loaded here.
+#' Import filesystem-related and standardization functions from the coviData
+#' package
 #'
-#' @importFrom coviData load_report_date path_create path_inv
-#'   path_pcr read_file_delim std_dates
-#'
+#' @md
 #' @name utils-coviData
+#' @keywords internal
+#' @importFrom coviData path_create path_by_pattern path_inv path_pcr
+#'   read_file_delim std_dates
 NULL
