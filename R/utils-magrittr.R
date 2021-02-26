b9724772376e2aa0c3f91d16ff2afe76ec42b2da
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-02-26 08:43:44 -0600

    Change `magrittr-helpers` to `utils-magrittr`

--- R/utils-magrittr.R
+++ R/utils-magrittr.R
@@ -1,12 +1,15 @@
-#' Magrittr Pipe Helpers
+#' Magrittr Pipe Utilities
 #'
 #' The magrittr pipe is a common programming tool in \strong{R} that often makes
 #' code much easier to read. In addition to the primary pipe operator, magrittr
 #' offers several additional pipes, as well as functions to help perform common
 #' infix-operations in a pipe-friendly manner.
 #'
+#' These functions are currently bulk imported to allow easy reading of code
+#' with the pipe operator. This decision may be re-thought at a later date.
+#'
 #' @md
-#' @name magrittr-helpers
+#' @name utils-magrittr
 #' @keywords internal
 #' @import magrittr
 NULL
