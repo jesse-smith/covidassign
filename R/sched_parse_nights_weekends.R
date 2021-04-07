3a37797c39309f8d4dbbd29fa9fe1c63738122af
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-04-07 08:55:17 -0500

    Standardize weekday column names

--- R/sched_parse_nights_weekends.R
+++ R/sched_parse_nights_weekends.R
@@ -17,14 +17,13 @@
 #' @export
 sched_parse_nights_weekends <- function(.data) {
 
-  weekdays <- sched_parse_weekday(
-    c("sun", "mon", "tue", "wed", "thu", "fri", "sat")
-  )
+  weekdays <- sched_parse_weekday(c("sun","mon","tue","wed","thu","fri","sat"))
 
   .data %>%
     dplyr::mutate(role = sched_std_names(.data[["role"]])) %>%
     dplyr::filter(.data[["role"]] == "Investigator") %>%
-    dplyr::select("member", c("sun", "mon", "tues", "wed", "thurs", "fri", "sat")) %>%
+    dplyr::rename_with(~ dplyr::coalesce(sched_parse_weekday(.x), .x)) %>%
+    dplyr::select("member", {{ weekdays }}) %>%
     dplyr::mutate(
       member = sched_std_names(.data[["member"]]),
       dplyr::across(!"member", ~ !is.na(.x))
