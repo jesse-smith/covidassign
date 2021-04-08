7b85d2559ffad83d58254b897317c5a6e4cfcd80
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-04-08 14:17:10 -0500

    Create REDcap download functions

--- R/download_redcap_cases.R
+++ R/download_redcap_cases.R
@@ -1,7 +1,7 @@
 #' Download Cases from Case Assignment REDcap Project
 #'
-#' `download_redcap_cases()` downloads records from the
-#' \strong{Case Assignment} REDcap project.
+#' `download_nit_records()` and `download_nca_records()` download records from
+#' the \strong{Interview Tool} and \strong{Case Assignment} REDcap projects.
 #'
 #' @section TODO:
 #' \enumerate{
@@ -11,9 +11,18 @@
 #'
 #' @param api_token The API token for the Case Assignment REDcap project
 #'
+#' @param fields `character`. The fields to download from the project.
+#'   By default, all fields are downloaded.
+#'
 #' @return A `tibble` with all variables in `character` format
-download_redcap_cases <- function(
-  api_token = Sys.getenv("redcap_NCA_token")
+#'
+#' @name download_redcap_records
+NULL
+
+#' @rdname download_redcap_records
+download_nca_records <- function(
+  api_token = Sys.getenv("redcap_NCA_token"),
+  fields = NULL
 ) {
 
   # URL base for API
@@ -24,7 +33,7 @@ download_redcap_cases <- function(
     content = "record",
     format = "json",
     type = "flat"
-  )
+  ) %>% append(redcap_fields(fields))
 
   httr::RETRY(
     "POST",
@@ -38,4 +47,47 @@ download_redcap_cases <- function(
     jsonlite::fromJSON() %>%
     dplyr::as_tibble() %>%
     dplyr::select(-dplyr::matches("^six$"))
+}
+
+#' @rdname download_redcap_records
+download_nit_records <- function(
+  api_token = Sys.getenv("redcap_NIT_token"),
+  fields = NULL
+) {
+  # URL base for API
+  api_uri <- "https://redcap.shelbycountytn.gov/api/"
+
+  api_params <- list(
+    token = api_token,
+    content = "record",
+    format = "json",
+    type = "flat"
+  ) %>% append(redcap_fields(fields))
+
+  httr::RETRY(
+    "POST",
+    api_uri,
+    body = api_params,
+    times = 12L,
+    pause_cap = 300L
+  ) %>%
+    httr::stop_for_status() %>%
+    httr::content(as = "text") %>%
+    jsonlite::fromJSON() %>%
+    dplyr::as_tibble()
+}
+
+#' Select REDcap Fields to Download
+#'
+#' `redcap_fields()` converts a `character` vector to a named `list` that can
+#' be appended to the REDcap API parameters
+#'
+#' @param fields `character`. REDcap fields to select.
+#'
+#' @return A named `list`
+redcap_fields <- function(fields) {
+  if (vec_is_empty(fields)) return(NULL)
+  fields %>%
+    set_names(paste0("fields[", seq_along(.) - 1L, "]")) %>%
+    as.list()
 }
